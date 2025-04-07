aa0dc4fd9f26363d41b76808949a3bd03be9ee74
Carlomadella <madella871@gmail.com>
2025-04-07 21:45:39 +0200

    [UPD] creata richiesta AJAX

--- js/script.js
+++ js/script.js
@@ -1,43 +1,11 @@
 // Milestone 3
 // Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 
-// Output chiamata AJAX
-photos = [
-{
-    "id": 1,
-    "title": "Skate Park",
-    "date": "01-07-2024",
-    "url": "https://marcolanci.it/boolean/assets/pictures/1.png"
-},
-{
-    "id": 2,
-    "title": "Passeggiata",
-    "date": "16-07-2024",
-    "url": "https://marcolanci.it/boolean/assets/pictures/2.png"
-},
-{
-    "id": 3,
-    "title": "Alpi",
-    "date": "01-07-2024",
-    "url": "https://marcolanci.it/boolean/assets/pictures/3.png"
-},
-{
-    "id": 4,
-    "title": "Sagra",
-    "date": "21-08-2024",
-    "url": "https://marcolanci.it/boolean/assets/pictures/4.png"
-},
-{
-    "id": 5,
-    "title": "Watergun",
-    "date": "23-08-2024",
-    "url": "https://marcolanci.it/boolean/assets/pictures/5.png"
-},
-{
-    "id": 6,
-    "title": "Riviera",
-    "date": "30-08-2024",
-    "url": "https://marcolanci.it/boolean/assets/pictures/6.png"
-}
-]
+const card =  document.getElementById("cards")
+
+// creata richiesta AJAX
+axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) =>{
+    // salvo in una variabile i dati che ricevo dalla richiesta API
+    let cardContent= resp.data
+})
 
